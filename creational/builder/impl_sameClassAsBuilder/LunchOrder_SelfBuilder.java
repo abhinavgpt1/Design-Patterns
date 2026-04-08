61d7e9bf727d4d3797ade0871040ee3379525554
abhinavgpt1 <abhinav200002@gmail.com>
2026-04-09 04:36:24 +0530

    Builder: impl folders (2) renamed

--- creational/builder/impl_sameClassAsBuilder/LunchOrder_SelfBuilder.java
+++ creational/builder/impl_sameClassAsBuilder/LunchOrder_SelfBuilder.java
@@ -1,7 +1,8 @@
 /**
  * This approach uses the same class as builder.
  * FYI, it's an early stage architecture decision which isn't flexible in
- * comparison to separate builder class approach which works well with legacy code.
+ * comparison to separate builder class approach which works well with legacy
+ * code.
  * 
  * There is no build() since the instantiation happens at the start.
  * No problem of duplicate properties. No Builder class to manage.
